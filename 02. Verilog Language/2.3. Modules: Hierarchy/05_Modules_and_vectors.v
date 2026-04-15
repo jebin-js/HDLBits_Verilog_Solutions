215de57cc16954d865d014e701917e29391b7023
JEBIN BENADICT J S <jebinben25@gmail.com>
2026-04-15 18:32:22 +0530

    Create 05_Modules_and_vectors.v

---
+++ 02. Verilog Language/2.3. Modules: Hierarchy/05_Modules_and_vectors.v
@@ -0,0 +1,23 @@
+module top_module ( 
+    input clk, 
+    input [7:0] d, 
+    input [1:0] sel, 
+    output [7:0] q 
+);
+    wire [7:0] w1, w2, w3;
+    
+    my_dff8 x1 (.clk (clk), .d(d), .q(w1));
+    my_dff8 x2 (.clk (clk), .d(w1), .q(w2));
+    my_dff8 x3 (.clk (clk), .d(w2), .q(w3));
+    
+    
+    always @(*)
+        case (sel)
+            2'b01: q=w1;
+            2'b10: q=w2;
+            2'b11: q=w3;
+            default: q=d;
+        endcase
+       
+
+endmodule

9997b8c76db3e38b617eaf5922740e535df85260
JEBIN BENADICT J S <jebinben25@gmail.com>
2026-07-09 13:03:20 +0530

    Create 14_Thermostat.v

---
+++ 03. Circuits/01. Combinational Logic/01. Basic Gates/14_Thermostat.v
@@ -0,0 +1,16 @@
+module top_module (
+    input too_cold,
+    input too_hot,
+    input mode,
+    input fan_on,
+    output heater,
+    output aircon,
+    output fan
+    
+); 
+
+    assign heater = (too_cold & mode);
+    assign aircon = ~mode & too_hot;
+    assign fan =  heater | aircon | fan_on;
+    
+endmodule
